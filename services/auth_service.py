49f65f8e68550b396ffdadeb3d439e3cd6ebbfa3
Daniel <rammzcl@gmail.com>
2026-05-27 01:28:04 +0000

    Stable backend with SQLite integration

--- services/auth_service.py
+++ services/auth_service.py
@@ -1,4 +1,7 @@
-from repositories.user_repository import UserRepository
+from sqlalchemy.orm import Session
+from database.connection import SessionLocal
+from database.models import User
+
 from factories.user_factory import UserFactory
 from strategies.sha256_strategy import SHA256Strategy
 from decorators.log_decorator import log_action
@@ -8,12 +11,14 @@ from observers.email_observer import EmailObserver
 from observers.logger_observer import LoggerObserver
 
 import secrets
+import re
+
 
 class AuthService:
 
     def __init__(self):
 
-        self.repository = UserRepository()
+        self.db: Session = SessionLocal()
 
         self.hash_strategy = SHA256Strategy()
 
@@ -22,45 +27,87 @@ class AuthService:
         self.subject.attach(EmailObserver())
         self.subject.attach(LoggerObserver())
 
-        self.users_file = "data/users.json"
+    def valid_email(self, email):
+
+        pattern = r"^[\w\.-]+@[\w\.-]+\.\w+$"
+
+        return re.match(pattern, email)
+
+    def valid_username(self, name):
+
+        return len(name) >= 3 and name.isalnum()
+
+    def valid_password(self, password):
+
+        return (
+            len(password) >= 8
+            and any(c.isupper() for c in password)
+            and any(c.isdigit() for c in password)
+        )
 
     @log_action
     def register(self, data):
 
-        users = self.repository.get_users(self.users_file)
+        if not self.valid_email(data.email):
+            return {"error": "Correo inválido"}
 
-        if data.email in users:
+        if not self.valid_username(data.name):
+            return {"error": "Nombre inválido"}
+
+        if not self.valid_password(data.password):
+            return {
+                "error":
+                "La contraseña debe tener 8 caracteres, una mayúscula y un número"
+            }
+
+        existing_user = (
+            self.db.query(User)
+            .filter(User.email == data.email)
+            .first()
+        )
+
+        if existing_user:
             return {"error": "Usuario ya existe"}
 
         hashed_password = self.hash_strategy.hash(data.password)
 
-        user = UserFactory.create(
+        user_data = UserFactory.create(
             data.name,
             data.email,
             hashed_password
         )
 
-        users[data.email] = user
+        new_user = User(
+            name=user_data["name"],
+            email=user_data["email"],
+            password=user_data["password"]
+        )
 
-        self.repository.save_users(self.users_file, users)
+        self.db.add(new_user)
 
-        self.subject.notify(user)
+        self.db.commit()
+
+        self.db.refresh(new_user)
+
+        self.subject.notify(user_data)
 
         return {"message": "Usuario registrado"}
 
     @log_action
     def login(self, data):
 
-        users = self.repository.get_users(self.users_file)
-
-        user = users.get(data.email)
+        user = (
+            self.db.query(User)
+            .filter(User.email == data.email)
+            .first()
+        )
 
         if not user:
             return {"error": "Usuario no encontrado"}
 
         hashed_password = self.hash_strategy.hash(data.password)
 
-        if user["password"] != hashed_password:
+        if user.password != hashed_password:
             return {"error": "Contraseña incorrecta"}
 
         token = secrets.token_hex(32)
@@ -68,7 +115,7 @@ class AuthService:
         return {
             "access_token": token,
             "user": {
-                "name": user["name"],
-                "email": user["email"]
+                "name": user.name,
+                "email": user.email
             }
         }
